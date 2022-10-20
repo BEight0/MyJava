d8ffd0f269bbbd58efa15cab4fef2d67f92ad64e
s5268452684s@gmail.com <Mick@MSI.r1803.tciii.org.tw>
2022-10-20 16:02:24 +0800

    commit

--- src/tw/brad/myjava/homework03.java
+++ src/tw/brad/myjava/homework03.java
@@ -1,9 +1,11 @@
 package tw.brad.myjava;
 
+import java.util.Scanner;
+
 public class homework03 {
 
 	public static void main(String[] args) {
-		Scanner scanner = 
+		Scanner scanner = new Scanner(System.in); 
 
 	}
 
