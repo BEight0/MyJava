86d2d2de06e9219122d3f0644ed266dc631ca159
s5268452684s@gmail.com <s5268452684s@gmail.com>
2022-10-21 11:13:37 +0800

    commit

--- src/tw/brad/myclass/MyDrawer.java
+++ src/tw/brad/myclass/MyDrawer.java
@@ -1,6 +1,7 @@
 package tw.brad.myclass;
 
 import java.awt.Color;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -8,4 +9,17 @@ public class MyDrawer extends JPanel{
 	public MyDrawer() {
 		setBackground(Color.yellow);
 	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		if(g instanceof Graphics) {
+			System.out.println("OK");
+		}else {
+		System.out.println("XX");
 }
+	}
+}
+
+
