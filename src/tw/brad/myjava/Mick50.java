6f77ac278fbd58797b50e24b0a7c4ed88f8ed279
s5268452684s@gmail.com <s5268452684s@gmail.com>
2022-10-24 22:15:05 +0800

    commit

--- src/tw/brad/myjava/Mick50.java
+++ src/tw/brad/myjava/Mick50.java
@@ -1,20 +1,21 @@
 package tw.brad.myjava;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Mick49 {
+public class Mick50 {
 
 	public static void main(String[] args) {
 		FileInputStream fin = null;
 		try {
-		     fin = new FileInputStream("dir1/file1.txt");
-		     int len; byte[] buf = new byte[3]; 
-		     while ( (len = fin.read(buf)) != -1) { //先給值在做比對的動作
-		    	 System.out.print(new String(buf, 0, len));
-		     }
-		     	     
+		    File file = new File("dir1/file1.txt"); 
+			fin = new FileInputStream(file);
+		    byte[] buf = new byte[(int)file.length()];
+		    fin.read(buf);
+		    System.out.println(new String(buf));
+		    
 		     fin.close();
 		     System.out.println("OK");
 		}catch(Exception fe) {
